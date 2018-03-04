460952d3b2ef3a5bab1ca64b5dcec01e60b5b33b
Gennadii Saltyshchak <GennadyGS@gmail.com>
2018-03-04 21:02:05 +0200

    Initial commit

---
+++ FsLogic.Experiments.Tests/Library.fs
@@ -0,0 +1,5 @@
+namespace FsLogic.Experiments.Tests
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ FsLogic.Experiments/Library.fs
@@ -0,0 +1,5 @@
+namespace FsLogic.Experiments
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
