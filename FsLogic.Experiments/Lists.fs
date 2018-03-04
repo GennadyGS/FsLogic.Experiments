5b7eccb5551d1ff1f348ca3a925e17d3a0beaa81
GennadyGS <GennadyGS@gmail.com>
2018-03-04 21:52:44 +0200

    Add modules

---
+++ FsLogic.Experiments/Lists.fs
@@ -0,0 +1,10 @@
+﻿module FsLogic.Experiments.Lists
+
+open FsLogic.Goal
+
+let rec membero x l = 
+       let (h, t, t2) = fresh()
+       matche l
+            [ cons x t2 ->> []
+              cons h t   ->> [ recurse (fun () -> membero x t) ]
+            ]
