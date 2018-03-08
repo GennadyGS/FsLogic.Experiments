5cc5db94263cc1c3aa8bcd77fa4bcdb16d365b77
GennadyGS <GennadyGS@gmail.com>
2018-03-08 14:21:19 +0200

    Fix tests

---
+++ FsLogic.Experiments.Tests/ListTests.fs
@@ -0,0 +1,22 @@
+﻿module FsLogic.Experiments.Tests.ListTests
+
+open Xunit
+open Swensen.Unquote
+open FsLogic.Substitution
+open FsLogic.Goal
+open FsLogic.Experiments.Lists
+
+[<Fact>]
+let ``membero should return singleton``() =
+    let res = run -1 (fun q -> membero q ~~[1])
+    res =! [ Det 1 ]
+
+[<Fact>]
+let ``membero should return empty for empty list``() = 
+    let res = run -1 (fun q -> membero q nil)
+    res =! []
+
+[<Fact>]
+let ``membero should return all items of list``() = 
+    let res = run -1 (fun q -> membero q ~~[1; 2; 3])
+    res =! [Det 1; Det 2; Det 3]
