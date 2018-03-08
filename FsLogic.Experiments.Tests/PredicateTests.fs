37c75c6506715268571badf6efc6507ae58fbb0b
Gennadii Saltyshchak <GennadyGS@gmail.com>
2018-03-08 16:34:38 +0200

    Add test for brothers

--- FsLogic.Experiments.Tests/PredicateTests.fs
+++ FsLogic.Experiments.Tests/PredicateTests.fs
@@ -1,9 +1,10 @@
 ﻿module Predicates
 
 open Xunit
+open Swensen.Unquote
+open FsLogic
 open FsLogic.Substitution
 open FsLogic.Goal
-open Swensen.Unquote
 open FsLogic.Experiments.Predicates
 
 let getValues<'T> =
@@ -11,7 +12,7 @@ let getValues<'T> =
     >> List.map (fun obj -> obj :?> 'T)
 
 let getSortedValues<'T when 'T : comparison> = 
-    getValues >> List.sort
+    getValues<'T> >> List.sort
 
 let internal fathero =
     binaryPredicate [
@@ -25,21 +26,16 @@ let internal grandFathero gf gc =
     let f = fresh()
     fathero gf f &&& fathero f gc
 
+let internal brother x y = 
+    let f = fresh()
+    fathero f x &&& fathero f y &&& x*<>*y
+
 let rec internal ancestor x y = 
     let z = fresh()
     fathero x y ||| (fathero x z &&& recurse (fun () -> ancestor z y))
 
-let f x y =
-    (x *=* ~~"Abraham" &&& y *=* ~~"Isaac")
-    ||| (x *=* ~~"Isaac" &&& y *=* ~~"Jacob")
-
 [<Fact>]
 let ``should infer result from simple facts`` () =
-    let res = run -1 (fun q -> f q ~~"Isaac")
-    res =! [ Det "Abraham" ]
-
-[<Fact>]
-let ``should infer result from simple facts 2`` () =
     let res = run -1 (fun q -> fathero q ~~"Isaac")
     res =! [ Det "Abraham" ]
 
@@ -52,3 +48,10 @@ let ``should infer result from rule``() =
 let ``should infer result from recursive rule`` () =
     let res = run -1 (fun q -> ancestor q ~~"Benjamin")
     res |> getSortedValues =! ([ "Jacob"; "Abraham"; "Isaac" ] |> List.sort)
+
+[<Fact>]
+let ``brothers should return distinct`` () =
+    let res = run -1 (fun q -> 
+        let (x, y) = fresh()
+        brother x y &&& q *=* ~~(x, y))
+    res |> getSortedValues<string*string> =! ([ ("Ismael", "Isaac"); ("Isaac", "Ismael") ] |> List.sort)
