3d1a88844a6717ad7f81cf42a71258cd1e1c6177
Gennadii Saltyshchak <GennadyGS@gmail.com>
2018-03-11 21:11:22 +0200

    Extract module GraphSearch.fs

--- FsLogic.Experiments.Tests/GraphSearchTests.fs
+++ FsLogic.Experiments.Tests/GraphSearchTests.fs
@@ -5,6 +5,7 @@ open Swensen.Unquote
 open FsLogic.Goal
 open FsLogic.Substitution
 open FsLogic.Experiments.Predicates
+open FsLogic.Experiments.GraphSearch
 
 let private graphLink = 
     binaryPredicate [
@@ -18,14 +19,7 @@ let private graphLink =
         ("f", "a")
     ]
 
-let rec private graphPath source target path = 
-    let medium, subPath = fresh()
-    conde [
-        [source *=* target; path *=* ~~[target]]
-        [graphLink source medium; recurse (fun () -> graphPath medium target subPath); path *=* cons source subPath]
-    ]
-
 [<Fact>]
 let ``graphPath should find first path`` () =
-    let res = run 1 (fun q -> graphPath ~~"a" ~~"c" q)
+    let res = run 1 (fun q -> graphPath graphLink ~~"a" ~~"c" q)
     res =! [ Det ["a"; "c"] ]
---
+++ FsLogic.Experiments/GraphSearch.fs
@@ -0,0 +1,10 @@
+﻿module FsLogic.Experiments.GraphSearch
+
+open FsLogic.Goal
+
+let rec graphPath graphLink source target path = 
+    let medium, subPath = fresh()
+    conde [
+        [source *=* target; path *=* ~~[target]]
+        [graphLink source medium; recurse (fun () -> graphPath graphLink medium target subPath); path *=* cons source subPath]
+    ]
