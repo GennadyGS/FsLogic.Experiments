4b87ef1ad56c686b335e07ab3716afdd82d62d2a
Gennadii Saltyshchak <GennadyGS@gmail.com>
2019-06-08 13:28:21 +0300

    Realign code

--- FsLogic.Experiments/Lists.fs
+++ FsLogic.Experiments/Lists.fs
@@ -3,20 +3,20 @@
 open FsLogic.Goal
 
 let rec membero item list = 
-       let (head, tail, tail2) = fresh()
-       matche list
-            [ cons item tail2 ->> []
-              cons head tail   ->> [ recurse (fun () -> membero item tail) ]]
+    let (head, tail, tail2) = fresh()
+    matche list [ 
+        cons item tail2 ->> []
+        cons head tail ->> [ recurse (fun () -> membero item tail) ]]
 
 let rec listNotLonger list1 list2 = 
     let head1, tail1, tail2 = fresh()
-    matche (list1, list2) 
-        [ (nil, __) ->> []
-          (cons head1 tail1, cons __ tail2) ->> [ recurse (fun () -> listNotLonger tail1 tail2) ]]
+    matche (list1, list2) [ 
+        (nil, __) ->> []
+        (cons head1 tail1, cons __ tail2) ->> [ recurse (fun () -> listNotLonger tail1 tail2) ]]
 
 let rec notMembero item list = 
     let (head, tail) = fresh()
-    matche list
-        [ nil ->> []
-          cons head tail ->> [ item *<>* head; recurse (fun () -> notMembero item tail) ]]
+    matche list [ 
+        nil ->> []
+        cons head tail ->> [ item *<>* head; recurse (fun () -> notMembero item tail) ]]
 
