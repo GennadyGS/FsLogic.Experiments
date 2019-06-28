f78bafb460b7e478f9d38369d5a7de170ecbe584
Gennadii Saltyshchak <GennadyGS@gmail.com>
2019-06-28 12:38:21 +0300

    Add experiments

--- FsLogic.Experiments.Tests/ParsingTests.fs
+++ FsLogic.Experiments.Tests/ParsingTests.fs
@@ -5,20 +5,75 @@ open Swensen.Unquote
 open FsLogic.Substitution
 open FsLogic.Goal
 
-let step list rest =
+let isStep step list rest =
     conde [
-        [list *=* cons ~~"up" rest]
-        [list *=* cons ~~"down" rest]
+        [list *=* cons ~~"up" rest; step *=* ~~"up"]
+        [list *=* cons ~~"down" rest; step *=* ~~"down"]
     ]
 
-let rec move list rest =
-    let list1 = fresh ()
+let rec isMove move list rest =
+    let list1, step1, steps1 = fresh ()
     conde [
-        [step list rest]
-        [step list list1; recurse (fun () -> move list1 rest)]
+        [isStep step1 list rest; move *=* cons step1 nil]
+        [isStep step1 list list1; recurse (fun () -> isMove steps1 list1 rest); move *=* cons step1 steps1]
     ]
+
+[<ReflectedDefinition>]
+let freshVars () = Unchecked.defaultof<'r>
+
+[<ReflectedDefinition>]
+let next (x : 'a list) = true
+
+let isStep2 step =
+    next ["up"] && step = "up" ||
+    next ["down"] && step = "down"
+
+let rec isMove2 move =
+    let step', move' = freshVars ()
+    isStep2 step' && move = [step'] ||
+    isStep2 step' && isMove2 move' && move = step' :: move'
+
+[<ReflectedDefinition>]
+let rec isList isItem list = 
+    let item', list' = freshVars ()
+    isItem item' && list = [item'] ||
+    isItem item' && isList isItem list' && list = item' :: list'
+
+[<ReflectedDefinition>]
+let isMove3 move = isList isStep2 move
 
 [<Fact>]
-let ``sentence should be parsed``() =
-    let res = run -1 (fun q -> move ~~["up"; "down"; "up"] nil)
-    res =! [ Free 0 ]
+let ``step should be parsed``() =
+    let res = run -1 (fun q -> isStep q ~~["up"] nil)
+    res =! [ Det "up" ]
+
+
+[<Fact>]
+let ``one-step move should be parsed``() =
+    let res = run -1 (fun q -> isMove q ~~["down"] nil)
+    res =! [ Det ["down"] ]
+
+[<Fact>]
+let ``move should be parsed``() =
+    let res = run -1 (fun q -> isMove q ~~["up"; "down"; "up"] nil)
+    res =! [ Det ["up"; "down"; "up"] ]
+
+[<Fact>]
+let ``sentence should be parsed 2``() =
+    let mkBody moves targetMoves = isMove moves ~~targetMoves nil
+    let res = run -1 (fun q -> mkBody q ["up"; "down"; "up"])
+    res =! [ Det ["up"; "down"; "up"] ]
+
+[<Fact>]
+let ``sentence should be parsed 3``() =
+    let res = run -1 (fun q -> 
+        let rec isList isItem resultList list rest = 
+            let list1, item1, list2 = fresh ()
+            conde [
+                [isItem item1 list rest; resultList *=* cons item1 nil]
+                [isItem item1 list list1; recurse (fun () -> isList isItem list2 list1 rest); resultList *=* cons item1 list2]
+            ]
+        let isMove move list rest = isList isStep move list rest
+        isMove q ~~["up"; "down"; "up"] nil
+    )
+    res =! [ Det ["up"; "down"; "up"] ]
